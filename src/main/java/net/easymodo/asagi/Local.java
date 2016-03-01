b1411f4977bd42ae4ad1c2096538f97c40f5046e
Antonizoon <antonizoon@bibanon.org>
2016-03-01 17:57:57 -0500

    bugfixes in external server patch
    [INFO] -------------------------------------------------------------
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[211,99] cannot find symbol
      symbol:   method substring(int,int)
      location: variable filename of type java.io.File
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[211,125] cannot find symbol
      symbol:   method substring(int,int)
      location: variable filename of type java.io.File
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[214,56] cannot find symbol
      symbol:   method openConnection()
      location: variable url of type java.lang.String
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[217,20] cannot find symbol
      symbol:   variable True
      location: class net.easymodo.asagi.Local
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[219,20] cannot find symbol
      symbol:   variable False
      location: class net.easymodo.asagi.Local
    [ERROR] /home/foolz/asagifix/src/main/java/net/easymodo/asagi/Local.java:[260,39] method onExternalServer in class net.easymodo.asagi.Local cannot be applied to given types;
      required: java.io.File,java.lang.String
      found: java.lang.String,java.lang.String
      reason: actual argument java.lang.String cannot be converted to java.io.File by method invocation conversion
    [INFO] 6 errors 
    [INFO] -------------------------------------------------------------

--- src/main/java/net/easymodo/asagi/Local.java
+++ src/main/java/net/easymodo/asagi/Local.java
@@ -208,15 +208,15 @@ public class Local extends Board {
     // Check if image already exists on external image host
     public boolean onExternalServer(File filename, String boardName) throws IOException {
         // ex: https://data.desustorage.org/a/image/1202/00/1202002714688.jpg
-        String url = String.format(this.externalServer + "/%s/image/%s/%s/%s", boardName, filename.substring(0, 4), filename.substring(4, 6), filename)
+        String url = String.format(this.externalServer + "/%s/image/%s/%s/%s", boardName, filename.getName().substring(0, 4), filename.substring(4, 6), filename);
         URL u = new URL(url);
 
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
         http.setConnectTimeout(5000); // set timeout to 5 seconds
         if (http.getResponseCode() == 200) { // HTTP 200 OK
-            return True;
+            return true;
         } else { // 404, 502, 301, whatever
-            return False;
+            return false;
         }
     }
 
@@ -257,7 +257,7 @@ public class Local extends Board {
         File outputFile = new File(outputDir + "/" + filename);
         try {
             // check if image already exists on external image server (for Desustorage)
-            if(outputFile.exists() || onExternalServer(filename, this.boardName)) {
+            if(outputFile.exists() || onExternalServer(filename.getName(), this.boardName)) {
                 if (!isPreview) outputFile.setLastModified(System.currentTimeMillis());
                 return;
             }
